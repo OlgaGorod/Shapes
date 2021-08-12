ce66f6343dfef930b66d6555ca721560dde2b4c6
Olga <olga7gorod@yandex.ru>
2021-08-12 14:05:34 +0300

    Add project files.

---
+++ GetShapes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Shapes;
+
+namespace GetShapesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sidesStr = new string[3];
+            string input;
+            double[] sides = new double[3];
+            bool haveSides = false; //1-стороны заданы, 0 - не заданы
+
+                
+            Console.WriteLine("Введите стороны треугольника через пробел");
+            input = Console.ReadLine();
+
+            try
+            {
+                    sidesStr = input.Split(" ");
+
+                for (int i = 0; i < 3; i++)
+                    sides[i] = Convert.ToDouble(sidesStr[i]);
+                haveSides = true;
+            }
+            catch 
+            { 
+                Console.WriteLine("Некорректный ввод"); 
+            }
+
+            if (haveSides)//если стороны заданы
+            {
+                Triangle tr = new Triangle(sides[0], sides[1], sides[2]);
+                tr.PrintArea();
+                tr.PrintPerimeter();
+                tr.PrintIfRightTriangle();
+            }
+
+            
+        }
+    }
+}
---
+++ Shapes/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Shapes
+{
+    /* abstract class Shape
+     {
+         protected double AreaOfShape;
+         protected double Perimeter;
+
+     }*/
+    public class Triangle //: Shape
+    {
+        double aSide, bSide, cSide;
+        double AreaOfShape;
+        double Perimeter;
+
+        public Triangle(double a, double b, double c)
+        {
+            aSide = a;
+            bSide = b;
+            cSide = c;
+        }
+
+        public double GetPerimeter()
+        {
+            if (aSide <= 0 || bSide <= 0 || cSide <= 0)
+            {
+                Console.WriteLine("Стороны должны быть положительным числом");
+                //throw new Exception("Стороны должны быть положительным числом");
+                return 0;
+            }
+
+            //сумма двух сторон треугольника всегда больше третьей стороны
+            if ((aSide + bSide) <= cSide || (aSide + cSide) <= bSide || (bSide + cSide) <= aSide)
+            {
+                Console.WriteLine("Треугольника с такими сторонами не существует");
+                //throw new Exception("Треугольника с такими сторонами не существует");
+                return 0;
+            }
+            
+            
+            return Perimeter = aSide + bSide + cSide;
+        }
+        public double GetArea()
+        {
+            double halfPerimeter = GetPerimeter() / 2;
+            if (Perimeter > 0)
+            {
+                AreaOfShape = Math.Sqrt(halfPerimeter * (halfPerimeter - aSide) * (halfPerimeter - bSide) *
+                    (halfPerimeter - cSide));
+            }
+            
+            return AreaOfShape;
+        }
+
+        public bool IfRightTriangle() // true - треугольник прямоугольный, false - нет
+        {
+                if (Math.Pow(cSide, 2) == Math.Pow(aSide, 2) + Math.Pow(bSide, 2))
+                    return true;
+                else return false;
+        }
+        public void PrintPerimeter()
+        {
+            if (GetPerimeter()>0)
+            Console.WriteLine($"Периметр треугольника равен {Perimeter}");
+        }
+        public void PrintArea()
+        {
+           if( GetArea()>0)
+            Console.WriteLine($"Площадь треугольника равна {AreaOfShape}");
+        }
+        public void PrintIfRightTriangle()
+        {
+            if (IfRightTriangle())
+                Console.WriteLine($"Треугольник со сторонами {aSide} {bSide} {cSide} является прямоугольным");
+            else if(Perimeter>0)
+                Console.WriteLine($"Треугольник со сторонами {aSide} {bSide} {cSide} НЕ является прямоугольным");
+        }
+        
+    }
+
+    public class Circle
+    {
+
+    }
+
+}
